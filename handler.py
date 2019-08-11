4f241898b1e8c4a25b85f54fc7604096ef5acd17
Tushar Verma <tushar.m.verma@gmail.com>
2019-08-11 19:50:49 -0400

    Add list post and get post

--- handler.py
+++ handler.py
@@ -3,3 +3,36 @@ import requests
 
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
+
+
+API_HOST = 'https://jsonplaceholder.typicode.com'
+
+
+def list_posts(event, context):
+    """ Retrieve posts list """
+    url = API_HOST + '/posts'
+
+    log.debug('calling ' + url)
+    res = requests.get(url)
+
+    response = {
+        "statusCode": res.status_code,
+        "body": res.text
+    }
+
+    return response
+
+
+def get_post(event, context):
+    """ Retrieve post by id """
+    url = API_HOST + '/posts/' + str(event['pathParameters']['id'])
+
+    log.debug('calling ' + url)
+    res = requests.get(url)
+
+    response = {
+        "statusCode": res.status_code,
+        "body": res.text
+    }
+
+    return response
