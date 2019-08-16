ea20f005c75bd82f88accdbe09adf3f754e88b8e
Tushar Verma <tushar.m.verma@gmail.com>
2019-08-16 19:52:32 -0400

    Initial test class

--- test.py
+++ test.py
@@ -2,3 +2,10 @@ import unittest
 import json
 from handler import list_posts, get_post
 
+
+class TestHandler(unittest.TestCase):
+    """ Tests handler methods """
+
+
+if __name__ == '__main__':
+    unittest.main()
