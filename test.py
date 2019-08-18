852b40c79d25fc70c9db28d1b3fa450c4d61ca16
Tushar Verma <tushar.m.verma@gmail.com>
2019-08-18 19:53:13 -0400

    test list

--- test.py
+++ test.py
@@ -6,6 +6,11 @@ from handler import list_posts, get_post
 class TestHandler(unittest.TestCase):
     """ Tests handler methods """
 
+    def test_list_posts(self):
+        """ Tests list_posts """
+        res = list_posts(None, None)
+        self.assertEquals(200, res['statusCode'])
+        self.assertTrue(len(res['body']) > 0)
 
 if __name__ == '__main__':
     unittest.main()
