8ca26ff565e08e8c21a8faea05c2c9cc013ed0ab
Tushar Verma <tushar.m.verma@gmail.com>
2019-08-19 19:53:45 -0400

    test post

--- test.py
+++ test.py
@@ -12,5 +12,19 @@ class TestHandler(unittest.TestCase):
         self.assertEquals(200, res['statusCode'])
         self.assertTrue(len(res['body']) > 0)
 
+    def test_get_post(self):
+        """ Tests get_post """
+        event = {}
+        event['pathParameters'] = {}
+        event['pathParameters']['id'] = "1"
+        res = get_post(event, None)
+
+        self.assertEquals(200, res['statusCode'])
+
+        post = json.loads(res['body'])
+        self.assertEquals(1, post['id'])
+        self.assertEquals("sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+         post['title'])
+
 if __name__ == '__main__':
     unittest.main()
